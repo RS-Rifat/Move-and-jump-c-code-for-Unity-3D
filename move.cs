9b12824aa9acf9697c6d97e3904917c1b38e7e31
MD Rifat Hossain <59175725+RS-Rifat@users.noreply.github.com>
2022-05-23 12:25:40 +0600

    Add files via upload

---
+++ move.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class move : MonoBehaviour
+{
+    public Rigidbody rb;
+
+    public float speed = 3f;
+    public float jumpForce = 10f;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        
+        float xDirection = Input.GetAxis("Horizontal");
+        float zDirection = Input.GetAxis("Vertical");
+
+        Vector3 moveDirecton = new Vector3(xDirection, 0.0f, zDirection);
+
+        transform.position += moveDirecton * speed;
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
+        }
+    }
+}
